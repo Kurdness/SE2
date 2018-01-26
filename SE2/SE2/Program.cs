3b7d88489f2c88f38c7b362c088b73ad40494a17
Student <Student@VMMB310-70>
2018-01-26 14:23:08 +0200

    First change

---
+++ SE2/SE2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is Software Engineering assignment number 2 !!!
+            // I havent created any branches yet... let test the changes first on this "master" branch.
+        }
+    }
+}
